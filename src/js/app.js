05cd00af3621cc7deed695834480220bcdbd8fe8
Maria <mariaes2016@yandex.ru>
2021-08-06 19:44:17 +0300

    unit2

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,20 @@ test('Checking the correctness of sorting', () => {
   expect(sortPlayerList(data)).toEqual(expected);
   expect(sortPlayerList(data)).not.toBe(expected);
 });
+
+export default function showHealth(units) {
+  let result;
+  if (units.health > 50) {
+    result = 'healthy';
+  }
+  if (units.health <= 50 && units.health > 15) {
+    result = 'wounded';
+  }
+  if (units.health <= 15 && units.health > 0) {
+    result = 'critical';
+  }
+  if (Number.isNaN(units.health) || units.health === undefined || units.health < 0 || typeof (units.health) !== 'number') {
+    throw new Error('Parameter is not a number!');
+  }
+  return result;
+}
