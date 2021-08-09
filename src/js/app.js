3e197996a03a7b404ba8605cd1027525afd6c5ae
Maria <mariaes2016@yandex.ru>
2021-08-09 08:16:48 +0300

    unit2

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,3 @@
-import sortPlayerList from '../js/app';
-
-test('Checking the correctness of sorting', () => {
-  const data = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-
- const expected = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-expect(sortPlayerList(data)).not.toBe(expected); 
-});
-test('sort to Equal success,' () => {
-  const data = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-
- const expected = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-  expect(sortPlayerList(data)).toEqual(expected);
-});
-
-
 export default function showHealth(units) {
   let result;
   if (units.health > 50) {
